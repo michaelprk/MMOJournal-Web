48edc7fa344e84d4bfe565ce97b4cca3ae008141
Michael Parke <michaelparke1994@gmail.com>
2025-07-04 14:01:37 +0100

    Update new component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,6 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+
 
 export function Navbar() {
   return (
@@ -6,30 +8,30 @@ export function Navbar() {
       <h1>MMOJournal-Web</h1>
       <ul style={{ display: "flex", gap: "1rem", listStyle: "none" }}>
         <li>
-          <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+          <NavLink to="/" style={{ color: "white", textDecoration: "none" }}>
             Home
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/pvp" style={{ color: "white", textDecoration: "none" }}>
+          <NavLink to="/pvp" style={{ color: "white", textDecoration: "none" }}>
             PvP Compendium
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
+          <NavLink
             to="/shiny-hunt"
             style={{ color: "white", textDecoration: "none" }}
           >
             Shiny Showcase
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
+          <NavLink
             to="/journey"
             style={{ color: "white", textDecoration: "none" }}
           >
             Journey Journal
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
